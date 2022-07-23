54afc75025b15dac1517d063d4418d32c2350e1f
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-07-23 12:28:40 +0200

    Time: 0 ms (100.00%), Space: 6.1 MB (16.57%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0)
-            return 0;
-        else if (n<3)
-            return 1;
-        else 
-            return fib(n-1)+fib(n-2);
+        vector<int> dp(n+1,-1);
+        return h3(n,dp);    
     }
+    //1 1  2 3 
+    int h3(int n,vector<int>&dp){
+        if(n==2)
+            return 1;
+        if(n<2)
+            return n;
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=h3(n-1,dp)+h3(n-2,dp);
+        
+    }
+    
 };
