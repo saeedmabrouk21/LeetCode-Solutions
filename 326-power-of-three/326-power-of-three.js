ccad33aee3a165bbf7c40b4a0cfff4bc5340b10c
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-08-24 07:56:48 +0200

    Time: 368 ms (34.27%), Space: 51 MB (76.24%) - LeetHub

--- 326-power-of-three/326-power-of-three.js
+++ 326-power-of-three/326-power-of-three.js
@@ -3,9 +3,12 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    let r = n.toString(3);
-    if(r.lastIndexOf(2)==-1 && r.lastIndexOf(1)==0 )
-        return true;
-    return false;
+    // let r = n.toString(3);
+     if(n<=0)
+            return false;
+        let z =Math.log10(n) / Math.log10(3);
+
+        return (z%1 ==0)
+    // return false;
     
 };
