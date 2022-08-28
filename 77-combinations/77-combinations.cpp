7a1c08aa676dc0e30eeb18bb225d7530caa411de
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-08-28 12:09:30 +0200

    Time: 1445 ms (5.01%), Space: 212.8 MB (5.06%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>res;
+        vector<int>perfix;
+        vector<int> ab(n);
+        for(int i = 1;i<=n;i++){
+            ab[i-1]=i;
+        }
+        perm(ab,res,perfix,k);
+        return res;
+    }
+    void perm(vector<int> ab,vector<vector<int>>& res,vector<int> perfix, int k){
+        if(perfix.size()==k){
+            res.push_back(perfix);
+            return;               
+        }
+        for(int i = 0 ; i < ab.size();i++){
+            perfix.push_back(ab[i]);
+
+            auto start = ab.begin() + i+1;
+            auto end = ab.end();
+            vector<int> result(ab.size() - i -1);
+            copy(start, end, result.begin());
+
+            perm(result,res,perfix,k);
+            perfix.pop_back();
+
+    }
+    }
+};
