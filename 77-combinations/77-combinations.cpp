9663817c95d0711fe0ce12c89ca069f26d033cf3
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-08-28 12:20:14 +0200

    Time: 513 ms (18.75%), Space: 49.7 MB (22.55%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>>res;
         vector<int>perfix;
         vector<int> ab(n);
         for(int i = 1;i<=n;i++){
             ab[i-1]=i;
         }
-        perm(ab,res,perfix,k);
-        return res;
+        return  perm(ab,perfix,k);
     }
-    void perm(vector<int> &ab,vector<vector<int>>& res,vector<int> &perfix, int &k){
+    vector<vector<int>> perm(vector<int> &ab,vector<int> &perfix, int &k){
+        vector<vector<int>> res;
         if(perfix.size()==k){
             res.push_back(perfix);
-            return;               
         }
         for(int i = 0 ; i < ab.size();i++){
             perfix.push_back(ab[i]);
-
             auto start = ab.begin() + i+1;
             auto end = ab.end();
             vector<int> result(ab.size() - i -1);
             copy(start, end, result.begin());
-
-            perm(result,res,perfix,k);
+            vector<vector<int>> second =perm(result,perfix,k);
+            res.insert(res.end(), second.begin(), second.end());
             perfix.pop_back();
-
+        }
+          return res;
     }
-    }
+  
 };
