80ee4220ff3d87c8b9402242366cf020f5cf98d0
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-08-29 18:16:01 +0200

    Time: 4 ms (79.51%), Space: 9 MB (99.90%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -5,15 +5,16 @@ public:
         int j;
         int i;
         for(j = 0 ; j < strs[0].size();j++){
-            s.push_back(strs[0][j]);
+            char x= strs[0][j];
             for(i = 0 ; i < strs.size();i++){
-                if(s!=strs[i].substr(0,j+1)){
-                    s.pop_back();
+                if(x!=strs[i][j]){
                     break;
                 }
             }
             if(i!=strs.size()){
                 break;
+            }else{
+                s.push_back(x);
             }
         }
         return s;
