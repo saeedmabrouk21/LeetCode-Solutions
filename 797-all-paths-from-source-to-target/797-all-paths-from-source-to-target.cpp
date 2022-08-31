bcce760342476769c3ca6c1bd937cf2c7954434e
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-08-31 16:08:44 +0200

    Time: 31 ms (40.54%), Space: 15.3 MB (33.58%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>>ans;
+        vector<int>perfix;
+        DFS(graph,ans,perfix,0);
+        return ans;
+    }
+    void DFS(vector<vector<int>> &graph,vector<vector<int>>&ans,vector<int>perfix,int i){
+        perfix.push_back(i);
+        if(i==graph.size()-1){
+            ans.push_back(perfix);
+            return;
+        }
+       
+        for(int j = 0 ; j < graph[i].size();j++){
+            
+            DFS(graph,ans,perfix,graph[i][j]);             
+        }
+    }
+    
+};
