4f34d584a927f34fbf1d8da366f80e35bec1b0f5
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-08-31 16:10:44 +0200

    Time: 14 ms (85.54%), Space: 10.5 MB (98.67%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -6,17 +6,18 @@ public:
         DFS(graph,ans,perfix,0);
         return ans;
     }
-    void DFS(vector<vector<int>> &graph,vector<vector<int>>&ans,vector<int>perfix,int i){
+    void DFS(vector<vector<int>> &graph,vector<vector<int>>&ans,vector<int>&perfix,int i){
         perfix.push_back(i);
         if(i==graph.size()-1){
             ans.push_back(perfix);
+            perfix.pop_back();
             return;
         }
        
         for(int j = 0 ; j < graph[i].size();j++){
-            
             DFS(graph,ans,perfix,graph[i][j]);             
         }
+        perfix.pop_back();
     }
     
 };
