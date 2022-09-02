028ea00f4c77339ecf350a573ec22773f593fb9d
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-09-02 11:05:57 +0200

    Time: 26 ms (12.33%), Space: 10.6 MB (94.67%) - LeetHub

---
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        
+        if(r*c != mat.size()*mat[0].size())
+            return mat;
+        vector<vector<int>> answer (r,vector<int>(c));
+        
+        vector< vector<int>>::iterator qq = mat.begin();
+        int v= 0 ;
+        for(int i = 0 ; i < r ;i++){
+            for(int j = 0 ; j < c ;j++){
+                if(v==mat[0].size()){
+                    v=0;
+                    qq++;
+                    }
+                answer[i][j]=(*qq)[v];
+                v++;
+            }
+        }
+        return answer;
+    }
+};
