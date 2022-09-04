96ec753b5163e507fc84a0633d7cb2be2d47a49e
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-09-04 08:28:14 +0200

    Time: 35 ms (5.25%), Space: 12.6 MB (32.63%) - LeetHub

---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> ab;
+        ab.push(root);
+        int next = 1;
+        vector<vector<int>>av;
+        if(!root)
+            return av;
+        while(!ab.empty()){
+            vector<int> cc;
+            cout<<"(())"<<"*&&*"<<next<<endl;
+            int d = next;
+            next = 0;
+            for(int i = 0 ; i < d ;i++){   
+                cc.push_back(ab.front()->val);
+                if(ab.front()->left){
+                    ab.push(ab.front()->left);
+                    cout<<"(())"<<d<<"**"<<next<<endl;
+                    next++;
+                    cout<<"(())"<<d<<"**"<<next<<endl;
+                }
+                if(ab.front()->right){
+                    ab.push(ab.front()->right);
+                    cout<<"(())"<<d<<"**"<<next<<endl;
+                    next++;
+                    cout<<"(())"<<d<<"**"<<next<<endl;
+                }
+                ab.pop();
+            }
+            av.push_back(cc);
+            
+        }
+        return av;
+    }
+};
