dd05f722a091e99cb6601f3f392904f370973f0f
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-09-08 14:37:37 +0200

    Time: 51 ms (5.54%), Space: 21.8 MB (30.77%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,17 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    
-    bool solve(TreeNode* root, long long int min,long long int max){
-        if(!root)return true;
-        
-        if(root->val>=max || root->val<=min)return false;
-        
-        return solve(root->left,min,root->val) and solve(root->right,root->val,max);
-    }
-    
     bool isValidBST(TreeNode* root) {
-        if(!root)return true;
+        TreeNode* tt=NULL;
+        return valid(root,tt);
+    }
+  
+    bool valid(TreeNode* root ,TreeNode* &tt){
+        bool a=true,b=true;
+        if(root->left)
+            a = valid(root->left,tt);
+        if(!a)return false;
+        if(!tt){
+            tt=root;
+        }else{
+            cout <<root->val <<" "<<tt->val<<endl;
+            if(root->val <= tt->val)
+                return false;
+            tt->val = root->val;
+        }
         
-        return solve(root,-1e18,1e18);
+        
+         if(root->right)
+            b = valid(root->right,tt);
+        
+        return a&&b;
+        
     }
 };
