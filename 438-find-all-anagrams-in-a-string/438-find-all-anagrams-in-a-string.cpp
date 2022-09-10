1811072ac19357572a57dde1f0228472c6565127
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-09-10 10:39:14 +0200

    Time: 852 ms (5.75%), Space: 8.7 MB (74.02%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -10,13 +10,13 @@ public:
             freq[p[i]-'a']++;
             
         }
-        
+        int t;
+        int freq2[26];
         for(int i = 0 ; i < s.size()-p.size()+1;i++){
-            int freq2[26];
             for(int k = 0 ; k < 26;k++){
                 freq2[k]=freq[k];
             }
-            int t = p.size();
+            t = p.size();
             for(int j = i ; j < i+p.size();j++){
                 if(freq2[s[j]-'a']>0){
                     freq2[s[j]-'a']--;
