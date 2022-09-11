5fe5cb93089e27104cc54b8a773eae141e2d5522
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-09-11 07:47:44 +0200

    Time: 283 ms (5.00%), Space: 7.1 MB (51.48%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {    
+        int maxx=k;
+        int arr[26]={0};
+        int start = 0 ;
+        int end = k;
+        for(int i = 0 ; i < k+1 ; i++){
+            arr[s[i]-'A']++;
+            
+        }
+        cout <<arr[0];
+        while(end<s.size()){
+            
+            int mostfrequent = mostt(arr);
+            cout <<endl<<end<<" "<<start<<" "<<mostfrequent<<" ";
+           if(k-(end-start+1-mostfrequent)<0){
+               maxx=max(maxx,end-start);
+               arr[s[start]-'A']--;
+               start++;
+               end++;
+               if(s[end])arr[s[end]-'A']++;
+           }else{
+               end++;
+               
+               if(s[end]){
+                   arr[s[end]-'A']++;
+               }
+           }
+        }
+        if(!s[end]) maxx=max(maxx,end-start);
+        return maxx;
+    }
+    int mostt(int arr[]){
+        int maxxx=0;
+        for(int i = 0 ;i < 26;i++){
+            if(arr[i]>maxxx)
+                maxxx=arr[i];
+        }
+        return maxxx;
+    }
+};
