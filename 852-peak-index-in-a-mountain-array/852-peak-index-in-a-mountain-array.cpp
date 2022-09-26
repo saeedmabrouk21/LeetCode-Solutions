a702cc2b11502286b710952c4347ababe465ea93
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-09-26 09:06:14 +0200

    Time: 300 ms (10.42%), Space: 59.7 MB (17.25%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -4,17 +4,17 @@ public:
         int start = 1  ;
         int end = arr.size()-2;
         int mid;
-        while(start<=end){
+        while(start<end){
             mid = start + (end - end)/2;
-            if(arr[mid-1]<arr[mid]&& arr[mid+1]<arr[mid])
-                return mid;
-            if (arr[mid-1]>arr[mid]&& arr[mid+1]<arr[mid]  ){
-                end=mid-1;
-            }else if(arr[mid-1]<arr[mid]&& arr[mid+1]>arr[mid]){
-                start = mid+1;
+            
+            if (arr[mid+1]>arr[mid]  ){
+                start=mid+1;
+            }else {
+                end = mid;
             }
             
         }
-        return 0;
+        
+        return start;
     }
 };
