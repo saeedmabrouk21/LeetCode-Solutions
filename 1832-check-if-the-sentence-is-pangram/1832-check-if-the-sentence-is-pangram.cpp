b72fcf2cb3e728cec88d8ed356a10fdb866b47ef
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-10-17 16:20:06 +0200

    Time: 3 ms (80.43%), Space: 6.5 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -5,14 +5,11 @@ public:
         int b = 0 ;
         for(int i = 0 ; i < sentence.length();i++){
             if(( a>> (sentence[i]-'a') & 1) == 0){
-              //  cout <<"da";
                 a = a | (1<<sentence[i]-'a');
-                //cout <<a;
-                b++;
             }
         }
         
-        if(b==26)
+        if(a==67108863)
             return true;
         return false;
     }
