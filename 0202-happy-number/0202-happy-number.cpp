9486a79efc2ee479c4fe979f81a62d125ddd2e44
Saeed Mabrouk <73306180+saeedmabrouk21@users.noreply.github.com>
2022-10-17 13:24:44 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (84.57%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_set<int> ab;
-        return test(n,ab);
-    }
-    bool test (int a ,unordered_set<int>& ab){
-        if(ab.count(a)==1)
-            return false;
-        ab.insert(a);
-        if(a == 1 )
-            return true;
-       
-        int x = 0 ;
-        while(a!=0){
-            x = x + (a%10)*(a%10);
-            a=a/10;
-        }
-        cout <<x<<endl;
-        return test(x,ab);
+        int slow = n;
+        int fast =n;
+        do{
+         
+            n= slow ;
+            slow = 0;
+            while(n!=0){
+                slow = slow + (n%10)*(n%10);
+                n=n/10;
+            }
+
+            n= fast;
+            fast = 0;
+            while(n!=0){
+                fast = fast + (n%10)*(n%10);
+                n=n/10;
+            }
+
+            n = fast;
+            fast = 0;
+            while(n!=0){
+                fast = fast + (n%10)*(n%10);
+                n=n/10;
+            }
+            if(fast ==1 )
+                return true;
+        }while(slow != fast );
+      
+        return false;
     }
 };
